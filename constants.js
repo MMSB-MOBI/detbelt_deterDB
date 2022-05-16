0b010cf2db3d62072c9f614b00e1b88c2aa54f70
Detbelt WS <cecile.hilpert@gmail.com>
2022-05-16 10:07:16 +0200

    constants deployment

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
-const DB_DIR = "/data/databases/mobi/detbelt/detergents/tingo"
+const DB_DIR = "/data/databases/detbelt/detergents/tingo"
 const DB_JSON_NAME = "tingoDet_legacy.json"
-const PDB_DIR = "/data/databases/mobi/detbelt/detergents/pdb_files"
-const PDB_IMAGE_DIR = "/data/databases/mobi/detbelt/detergents/pdb_images"
+const PDB_DIR = "/data/databases/detbelt/detergents/pdb_files"
+const PDB_IMAGE_DIR = "/data/databases/detbelt/detergents/pdb_images"
 
 module.exports = {
     DB_DIR : DB_DIR,
